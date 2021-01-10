549c73d42f4b92692b5c5fc230523da87331c9fd
Lucasmaciiel <lucasmacielgois@gmail.com>
2021-01-10 20:11:24 -0300

    Add HomePage

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
+import Home from './Home';
 import './Navbar/index'
 import Navbar from './Navbar/index';
 
 function App() {
   return (
-    <div className="App">
+      <>
        <Navbar />
-    </div>
+       <Home />
+    </>
   );
 }
 
