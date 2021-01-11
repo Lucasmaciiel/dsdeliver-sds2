4942847a2f5d4f6eb675d41c65369aa47469e44c
Lucasmaciiel <lucasmacielgois@gmail.com>
2021-01-11 20:35:20 -0300

    Correção commit

---
+++ front-web/src/Orders/ProductCard.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function ProductCard() {
+	return (
+		<div className='orders-card-container'>
+			<h3 className="order-card-title">
+                Pizza Calabresa
+            </h3>
+            
+		</div>
+	)
+}
+
+export default ProductCard;
--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -2,11 +2,13 @@ import './styles.css';
 import { ReactComponent as Logo} from './logo.svg'
 import React from "react";
 import StepsHeader from './StepsHeader';
+import ProductsList from './ProductsList';
 
 function Orders() {
     return (
         <div className="orders-container">
           <StepsHeader />
+          <ProductsList />
         </div>
     )
 }
